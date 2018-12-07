b1379a951c8919b2ea59e5ffb49610c8e660a6fd
zengfansheng <zeng_fs@yeah.net>
2018-12-07 16:38:32 +0800

    使用新API获取签名信息

--- library/src/main/java/com/zfs/commons/utils/SignUtils.java
+++ library/src/main/java/com/zfs/commons/utils/SignUtils.java
@@ -3,6 +3,8 @@ package com.zfs.commons.utils;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Build;
 
 import java.util.List;
 
@@ -21,7 +23,26 @@ public class SignUtils {
     public static String getSignatureFromApk(Context context, String apkPath, String algorithm) {
         return getSignatureFromApk(context, apkPath, algorithm, "");
     }
-    
+
+    private static String getSignature(Signature signature, String algorithm, String separator) {
+        String code = EncryptUtils.encryptByMessageDigest(signature.toByteArray(), algorithm);
+        return EncryptUtils.addSeparator(code, separator);
+    }
+
+    private static String getSignature(PackageInfo info, String algorithm, String separator) {
+        Signature signature;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            if (info.signingInfo.hasMultipleSigners()) {
+                signature = info.signingInfo.getApkContentsSigners()[0];
+            } else {
+                signature = info.signingInfo.getSigningCertificateHistory()[0];
+            }
+        } else {
+            signature = info.signatures[0];
+        }
+        return getSignature(signature, algorithm, separator);
+    }
+
     /**
      * 从APK中读取签名
      *
@@ -29,16 +50,18 @@ public class SignUtils {
      * @param separator 用来分隔的字符串
      */
     public static String getSignatureFromApk(Context context, String apkPath, String algorithm, String separator) {
-        PackageInfo packageSign = context.getPackageManager().getPackageArchiveInfo(apkPath, PackageManager.GET_SIGNATURES);
-        if (packageSign != null) {
-            try {
-                String code = EncryptUtils.encryptByMessageDigest(packageSign.signatures[0].toByteArray(), algorithm);
-                if (code != null) {
-                    return EncryptUtils.addSeparator(code, separator);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+        try {
+            PackageInfo packageSign;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                PackageInfo info = context.getPackageManager().getPackageArchiveInfo(apkPath, PackageManager.GET_SIGNING_CERTIFICATES);
+                packageSign = context.getPackageManager().getPackageInfo(info.packageName, PackageManager.GET_SIGNING_CERTIFICATES);
+                return getSignature(packageSign, algorithm, separator);
+            } else {
+                packageSign = context.getPackageManager().getPackageArchiveInfo(apkPath, PackageManager.GET_SIGNATURES);
+                return getSignature(packageSign, algorithm, separator);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
@@ -58,13 +81,15 @@ public class SignUtils {
      */
     public static String getSignatureInstalled(Context context, String packageName, String algorithm, String separator) {
         try {
-            List<PackageInfo> infos = context.getPackageManager().getInstalledPackages(PackageManager.GET_SIGNATURES);
+            List<PackageInfo> infos;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                infos = context.getPackageManager().getInstalledPackages(PackageManager.GET_SIGNING_CERTIFICATES);
+            } else {
+                infos = context.getPackageManager().getInstalledPackages(PackageManager.GET_SIGNATURES);
+            }
             for (PackageInfo info : infos) {
                 if (info.packageName.equals(packageName)) {
-                    String code = EncryptUtils.encryptByMessageDigest(info.signatures[0].toByteArray(), algorithm);
-                    if (code != null) {
-                        return EncryptUtils.addSeparator(code, separator);
-                    }
+                    return getSignature(info, algorithm, separator);
                 }
             }
         } catch (Exception e) {
