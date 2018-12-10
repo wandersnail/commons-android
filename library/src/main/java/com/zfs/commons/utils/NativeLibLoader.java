c18cf7ac36b1ef2980f147b255eeb89e0b31222d
zengfansheng <zeng_fs@yeah.net>
2018-12-10 11:25:34 +0800

    新增动态加载so库工具类

---
+++ library/src/main/java/com/zfs/commons/utils/NativeLibLoader.java
@@ -0,0 +1,104 @@
+package com.zfs.commons.utils;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述: so库动态加载工具类
+ * 时间: 2018/12/10 11:17
+ * 作者: zengfansheng
+ */
+public class NativeLibLoader {
+
+    public static synchronized void installLibrary(ClassLoader classLoader, File folder) throws Throwable {
+        if (folder == null || !folder.exists()) {
+            return;
+        }
+        // android o sdk_int 26
+        // for android o preview sdk_int 25
+        if ((Build.VERSION.SDK_INT == 25 && getPreviousSdkInt() != 0) || Build.VERSION.SDK_INT > 25) {
+            try {
+                V25.install(classLoader, folder);
+            } catch (Throwable throwable) {
+                // install fail, try to treat it as v23
+                // some preview N version may go here
+                V23.install(classLoader, folder);
+            }
+        } else if (Build.VERSION.SDK_INT >= 23) {
+            try {
+                V23.install(classLoader, folder);
+            } catch (Throwable throwable) {
+                // install fail, try to treat it as v14
+                V14.install(classLoader, folder);
+            }
+        }
+    }
+
+    /**
+     * fuck部分机型删了该成员属性，兼容
+     *
+     * @return 被厂家删了返回1，否则正常读取
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private static int getPreviousSdkInt() {
+        try {
+            return Build.VERSION.PREVIEW_SDK_INT;
+        } catch (Throwable ignore) {
+        }
+        return 1;
+    }
+
+    private static final class V14 {
+        private static void install(ClassLoader classLoader, File folder) throws Throwable {
+            Field pathListField = ShareReflectUtil.findField(classLoader, "pathList");
+            Object dexPathList = pathListField.get(classLoader);
+            ShareReflectUtil.expandFieldArray(dexPathList, "nativeLibraryDirectories", new File[]{folder});
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static final class V23 {
+        private static void install(ClassLoader classLoader, File folder) throws Throwable {
+            Field pathListField = ShareReflectUtil.findField(classLoader, "pathList");
+            Object dexPathList = pathListField.get(classLoader);
+            Field nativeLibraryDirectories = ShareReflectUtil.findField(dexPathList, "nativeLibraryDirectories");
+            List<File> libDirs = (List<File>) nativeLibraryDirectories.get(dexPathList);
+            libDirs.add(0, folder);
+            Field systemNativeLibraryDirectories = ShareReflectUtil.findField(dexPathList, "systemNativeLibraryDirectories");
+            List<File> systemLibDirs = (List<File>) systemNativeLibraryDirectories.get(dexPathList);
+            Method makePathElements = ShareReflectUtil.findMethod(dexPathList, "makePathElements", List.class, File.class, List.class);
+            ArrayList<IOException> suppressedExceptions = new ArrayList<>();
+            libDirs.addAll(systemLibDirs);
+            Object[] elements = (Object[]) makePathElements.invoke(dexPathList, libDirs, null, suppressedExceptions);
+            Field nativeLibraryPathElements = ShareReflectUtil.findField(dexPathList, "nativeLibraryPathElements");
+            nativeLibraryPathElements.setAccessible(true);
+            nativeLibraryPathElements.set(dexPathList, elements);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static final class V25 {
+        private static void install(ClassLoader classLoader, File folder) throws Throwable {
+            Field pathListField = ShareReflectUtil.findField(classLoader, "pathList");
+            Object dexPathList = pathListField.get(classLoader);
+            Field nativeLibraryDirectories = ShareReflectUtil.findField(dexPathList, "nativeLibraryDirectories");
+            List<File> libDirs = (List<File>) nativeLibraryDirectories.get(dexPathList);
+            libDirs.add(0, folder);
+            Field systemNativeLibraryDirectories = ShareReflectUtil.findField(dexPathList, "systemNativeLibraryDirectories");
+            List<File> systemLibDirs = (List<File>) systemNativeLibraryDirectories.get(dexPathList);
+            Method makePathElements = ShareReflectUtil.findMethod(dexPathList, "makePathElements", List.class);
+            libDirs.addAll(systemLibDirs);
+            Object[] elements = (Object[]) makePathElements.invoke(dexPathList, libDirs);
+            Field nativeLibraryPathElements = ShareReflectUtil.findField(dexPathList, "nativeLibraryPathElements");
+            nativeLibraryPathElements.setAccessible(true);
+            nativeLibraryPathElements.set(dexPathList, elements);
+        }
+    }
+}
