fe21e732a78520e32f8395ef24662fb5287c3b92
fszeng2011 <zeng_fs@yeah.net>
2018-12-10 23:26:37 +0800

    完善解压缩工具

--- library/src/main/java/com/zfs/commons/entity/ZipHelper.java
+++ library/src/main/java/com/zfs/commons/entity/ZipHelper.java
@@ -22,15 +22,15 @@ public class ZipHelper {
     private ZipHelper() {
     }
 
-    public static ZipBuilder zip() {
-        return new ZipBuilder();
+    public static ZipExecutor zip() {
+        return new ZipExecutor();
     }
 
-    public static UnzipBuilder unzip() {
-        return new UnzipBuilder();
+    public static UnzipExecutor unzip() {
+        return new UnzipExecutor();
     }
 
-    public static class ZipBuilder {
+    public static class ZipExecutor {
         private String comment;
         private int method = -1;
         private int level = -1;
@@ -38,10 +38,10 @@ public class ZipHelper {
         private String targetPath;
         private boolean replace;
 
-        private ZipBuilder() {
+        private ZipExecutor() {
         }
 
-        public ZipBuilder setComment(String comment) {
+        public ZipExecutor setComment(String comment) {
             this.comment = comment;
             return this;
         }
@@ -51,7 +51,7 @@ public class ZipHelper {
          *
          * @param method {@link ZipEntry#STORED}, {@link ZipEntry#DEFLATED}
          */
-        public ZipBuilder setMethod(int method) {
+        public ZipExecutor setMethod(int method) {
             if (method == ZipEntry.STORED || method == ZipEntry.DEFLATED) {
                 this.method = method;
             }
@@ -63,7 +63,7 @@ public class ZipHelper {
          *
          * @param level 0~9
          */
-        public ZipBuilder setLevel(int level) {
+        public ZipExecutor setLevel(int level) {
             if (level > 9) {
                 this.level = 9;
             } else {
@@ -75,7 +75,7 @@ public class ZipHelper {
         /**
          * 添加待压缩文件
          */
-        public ZipBuilder addSourceFile(@NonNull File file) {
+        public ZipExecutor addSourceFile(@NonNull File file) {
             if (file.exists() && !files.contains(file)) {
                 files.add(file);
             }
@@ -85,7 +85,7 @@ public class ZipHelper {
         /**
          * 压缩包保存路径
          */
-        public ZipBuilder setTargetPath(String targetPath) {
+        public ZipExecutor setTargetPath(String targetPath) {
             this.targetPath = targetPath;
             return this;
         }
@@ -93,7 +93,7 @@ public class ZipHelper {
         /**
          * 目标路径下已存在同名压缩包，是否替换
          */
-        public ZipBuilder setReplace(boolean replace) {
+        public ZipExecutor setReplace(boolean replace) {
             this.replace = replace;
             return this;
         }
@@ -207,19 +207,20 @@ public class ZipHelper {
         }
     }
 
-    public static class UnzipBuilder {
+    public static class UnzipExecutor {
         private List<File> zipFiles = new ArrayList<>();
         private String targetDir;
 
+        private UnzipExecutor() {}
         
-        public UnzipBuilder addZipPath(@NonNull File zipFile) {
+        public UnzipExecutor addZipPath(@NonNull File zipFile) {
             if (zipFile.exists() && !zipFiles.contains(zipFile)) {
                 zipFiles.add(zipFile);
             }
             return this;
         }
 
-        public UnzipBuilder setTargetDir(String targetDir) {
+        public UnzipExecutor setTargetDir(String targetDir) {
             this.targetDir = targetDir;
             return this;
         }
@@ -227,7 +228,7 @@ public class ZipHelper {
         /**
          * 执行解压，同步的
          */
-        public boolean execute() {
+        public boolean execute() {            
             if (zipFiles.isEmpty()) {
                 return false;
             } else {
