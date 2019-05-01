298f492011e58ff6574728290a2c10c52081b2e2
fszeng2011 <zeng_fs@yeah.net>
2019-05-01 18:43:16 +0800

    删除java库

--- java/src/main/java/com/snail/java/network/exception/ConvertException.kt
+++
@@ -1,9 +0,0 @@
-package com.snail.java.network.exception
-
-/**
- * 转换异常
- *
- * date: 2019/2/26 12:29
- * author: zengfansheng
- */
-class ConvertException(message: String?, cause: Throwable?) : Exception(message, cause)
